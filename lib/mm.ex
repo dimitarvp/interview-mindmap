2ed8942aff676cd2f4ce2ce95b0a4dc5baba61be
Dimitar Panayotov <mitko.p@gmail.com>
2020-06-09 16:25:56 +0300

    Cleanup generator boilerplate

--- lib/mm.ex
+++ lib/mm.ex
@@ -1,18 +1,4 @@
 defmodule MM do
   @moduledoc """
-  Documentation for `MM`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> MM.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/mm_test.exs
+++ test/mm_test.exs
@@ -1,8 +1,4 @@
 defmodule MMTest do
   use ExUnit.Case
   doctest MM
-
-  test "greets the world" do
-    assert MM.hello() == :world
-  end
 end
