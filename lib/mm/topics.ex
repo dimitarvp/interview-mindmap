46adb0e660e4bba0469bafa4eadb19bf5fac9fbb
Dimitar Panayotov <mitko.p@gmail.com>
2020-06-11 01:39:08 +0300

    Rename variable to make it clearer

--- lib/mm/topics.ex
+++ lib/mm/topics.ex
@@ -3,10 +3,10 @@ defmodule MM.Topics do
   alias MM.Schema.Topic
   import Ecto.Query
 
-  def roots_by_user_id(id) do
+  def roots_by_user_id(user_id) do
     from(
       x in Topic,
-      where: x.user_id == ^id,
+      where: x.user_id == ^user_id,
       where: is_nil(x.parent_id),
       order_by: [asc: :inserted_at]
     )
