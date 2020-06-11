4685a38e1177d4041fed911fe15cd600809cc291
Dimitar Panayotov <mitko.p@gmail.com>
2020-06-11 04:42:32 +0300

    Add level 0 & 1 & 2 search for names

--- lib/mm/topics.ex
+++ lib/mm/topics.ex
@@ -12,4 +12,50 @@ defmodule MM.Topics do
     )
     |> Repo.all()
   end
+
+  def search(user_id, parent_id, name) do
+    search_query(user_id, parent_id, name)
+    |> Repo.all()
+  end
+
+  def search_query(user_id, nil, name) do
+    level_one = level_one_query(user_id, nil)
+
+    from(
+      x in Topic,
+      where: is_nil(x.id) or is_nil(x.parent_id) or x.parent_id in subquery(level_one),
+      where: x.user_id == ^user_id,
+      where: ilike(x.name, ^"%#{name}%"),
+      order_by: [asc: :inserted_at]
+    )
+  end
+
+  def search_query(user_id, parent_id, name) do
+    level_one = level_one_query(user_id, parent_id)
+
+    from(
+      x in Topic,
+      where:
+        x.id == ^parent_id or x.parent_id == ^parent_id or x.parent_id in subquery(level_one),
+      where: x.user_id == ^user_id,
+      where: ilike(x.name, ^"%#{name}%"),
+      order_by: [asc: :inserted_at]
+    )
+  end
+
+  def level_one_query(nil, nil) do
+    from(x in Topic, where: is_nil(x.parent_id), select: [:id])
+  end
+
+  def level_one_query(nil, parent_id) do
+    from(x in Topic, where: x.parent_id == ^parent_id, select: [:id])
+  end
+
+  def level_one_query(user_id, nil) do
+    from(x in Topic, where: x.user_id == ^user_id and is_nil(x.parent_id), select: [:id])
+  end
+
+  def level_one_query(user_id, parent_id) do
+    from(x in Topic, where: x.user_id == ^user_id and x.parent_id == ^parent_id, select: [:id])
+  end
 end
--- test/mm_test.exs
+++ test/mm_test.exs
@@ -41,5 +41,55 @@ defmodule MMTest do
     assert expected_names == actual_names
   end
 
-  defp names(list), do: Enum.map(list, & Map.get(&1, :name))
+  test "searching" do
+    u1 = insert(:user)
+    _u2 = insert(:user)
+
+    t1 = insert(:topic, user: u1, name: "Call")
+    t2 = insert(:topic, user: u1, name: "Toy")
+    t3 = insert(:topic, user: u1, name: "Right")
+
+    _t1_1 = insert(:topic, user: u1, parent: t1, name: "Hello")
+    _t1_2 = insert(:topic, user: u1, parent: t1, name: "Hallo")
+    _t1_3 = insert(:topic, user: u1, parent: t1, name: "Ollo")
+
+    _t2_1 = insert(:topic, user: u1, parent: t2, name: "Night")
+    _t2_2 = insert(:topic, user: u1, parent: t2, name: "Dwight")
+    _t2_3 = insert(:topic, user: u1, parent: t2, name: "Boy")
+
+    _t3_1 = insert(:topic, user: u1, parent: t3, name: "Soy")
+    _t3_2 = insert(:topic, user: u1, parent: t3, name: "Wight")
+    _t3_3 = insert(:topic, user: u1, parent: t3, name: "Might")
+
+    assert ~w(Call Hello Hallo Ollo) ==
+             search_names(u1.id, nil, "ll")
+
+    assert ~w(Toy Boy Soy) ==
+             search_names(u1.id, nil, "oy")
+
+    assert ~w(Right Night Dwight Wight Might) ==
+             search_names(u1.id, nil, "ight")
+
+    assert ~w(Hello Hallo Ollo) ==
+             search_names(u1.id, t1.id, "llo")
+
+    assert ~w(Night Dwight) ==
+             search_names(u1.id, t2.id, "ight")
+
+    assert ~w(Toy Boy) ==
+             search_names(u1.id, t2.id, "oy")
+
+    assert ~w(Right Wight Might) ==
+             search_names(u1.id, t3.id, "ight")
+
+    assert ~w(Soy) ==
+             search_names(u1.id, t3.id, "oy")
+  end
+
+  defp names(list), do: Enum.map(list, &Map.get(&1, :name))
+
+  defp search_names(user_id, parent_id, name) do
+    Topics.search(user_id, parent_id, name)
+    |> names()
+  end
 end
